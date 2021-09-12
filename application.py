f4ba92cf943a226eb36a10918cef5757b5ed6de6
Jesse Zahrt <jzahrt@hy-vee.com>
2021-09-12 16:38:57 -0500

    change region and app
    change region and application name

--- application.py
+++ application.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 # @app.route('/')
 # def hello_world():
 #     return 'Hello World!'
 
-@app.route("/")
+
+@application.route("/")
 def homepage():
     return render_template("index.html", h1="NO")
 
 
-@app.route("/docs")
+@application.route("/docs")
 def docs():
     return render_template("docs.html", title="docs page")
 
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html", title="about page")
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    application.run()
